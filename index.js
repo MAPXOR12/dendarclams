5c8229aff4bda56d25d85c6cde4e7e5f7a4bef9d
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2022-01-14 17:50:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('OTE2Nzc2NDgzMzY2NzcyNzU2.YavEog.i2zwPxjyMgXlc9i49s_4fdij7Lc');
+client.login('OTE2Nzc2NDgzMzY2NzcyNzU2.YavEog.P_G3lNu4v8OQX0mvo7NdPkUxR3g');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
